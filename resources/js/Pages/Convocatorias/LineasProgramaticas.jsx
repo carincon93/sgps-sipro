10e5281130cac7eda4c8c3797bb40b2656d4eab9
carincon93 <carincon93@gmail.com>
2023-08-08 09:08:50 -0500

    Convocatoria activadores added

--- resources/js/Pages/Convocatorias/LineasProgramaticas.jsx
+++ resources/js/Pages/Convocatorias/LineasProgramaticas.jsx
@@ -1,7 +1,10 @@
-import ButtonMui from '@/Components/Button'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+
+import AlertMui from '@/Components/Alert'
+import ButtonMui from '@/Components/Button'
+
 import { route, checkRole } from '@/Utils'
-import { router } from '@inertiajs/react'
+import { Link, router } from '@inertiajs/react'
 import { Grid } from '@mui/material'
 
 const ConvocatoriaLineasProgramaticas = ({ auth, convocatoria }) => {
@@ -13,6 +16,18 @@ const ConvocatoriaLineasProgramaticas = ({ auth, convocatoria }) => {
             <Grid item md={12}>
                 <h1 className="text-4xl text-center mb-8">A continuación, se listan las líneas programáticas de la vigencia {convocatoria.year} en las que puede formular proyectos.</h1>
             </Grid>
+
+            {checkRole(auth_user, [1, 20, 18, 19, 5, 17]) && (
+                <Grid item md={12}>
+                    <AlertMui className="mt-10">
+                        <p>Si desea modificar la convocatoria por favor de clic en el siguiente botón:</p>
+                        <Link href={route('convocatorias.edit', [convocatoria.id])} className="my-4 bg-app-800 text-white py-2 px-4 rounded inline-block">
+                            Modificar convocatoria
+                        </Link>
+                    </AlertMui>
+                </Grid>
+            )}
+
             {convocatoria.lineas_programaticas.map((linea_programatica) => (
                 <Grid item md={3} key={linea_programatica.id}>
                     <ButtonMui
