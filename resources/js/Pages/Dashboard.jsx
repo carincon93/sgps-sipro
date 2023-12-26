ea2a7ddea0b173cf4bd70e154b8b764793553385
Cristian <carincon93@gmail.com>
2023-12-26 14:14:44 -0500

    Estados evaluaciones modified

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -103,15 +103,13 @@ export default function Dashboard({ auth, roles_sistema }) {
                     </Link>
                 </Grid>
 
-                {checkRole(auth_user, [1, 5, 17, 18, 19]) && (
-                    <Grid item md={4}>
-                        <Link
-                            className="bg-white overflow-hidden rounded-lg px-6 py-2 hover:bg-app-800 hover:text-white h-[200px] shadow-md flex justify-around items-center flex-col"
-                            href={route('proyectos.index')}>
-                            Resultados proyectos SGPS
-                        </Link>
-                    </Grid>
-                )}
+                <Grid item md={4}>
+                    <Link
+                        className="bg-white overflow-hidden rounded-lg px-6 py-2 hover:bg-app-800 hover:text-white h-[200px] shadow-md flex justify-around items-center flex-col"
+                        href={route('proyectos.index')}>
+                        Resultados proyectos SGPS
+                    </Link>
+                </Grid>
                 {checkRole(auth_user, [1, 5, 11, 17, 18, 19, 33]) && (
                     <Grid item md={4}>
                         <Link
